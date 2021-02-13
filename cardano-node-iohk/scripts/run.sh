bc546106596dbe25c32bcbffc42b421ba0ef07a0
Bourke Floyd <chbfiv@gmail.com>
2021-02-13 09:26:33 -0800

    complete re-write to use debian instead of alpine (due to ghc/cardano build dependencies)

--- cardano-node-iohk/scripts/run.sh
+++ cardano-node-iohk/scripts/run.sh
@@ -6,13 +6,13 @@ set -o pipefail
 PARAM=$1
 
 if [[ "$PARAM" == "mainnet" ]]; then
-  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=mainnet -p 3001:3001 floydcraft/cardano-node:latest
+  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=mainnet -p 3001:3001 floydcraft/cardano-node-iohk:latest
 elif [[ "$PARAM" == "testnet" ]]; then
-  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=testnet -p 3001:3001 floydcraft/cardano-node:latest
+  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=testnet -p 3001:3001 floydcraft/cardano-node-iohk:latest
 elif [[ "$PARAM" == "mainbash" ]]; then
-  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=mainnet -p 3001:3001 --entrypoint bash floydcraft/cardano-node:latest
+  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=mainnet -p 3001:3001 --entrypoint bash floydcraft/cardano-node-iohk:latest
 elif [[ "$PARAM" == "testbash" ]]; then
-  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=testnet -p 3001:3001 --entrypoint bash floydcraft/cardano-node:latest
+  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=testnet -p 3001:3001 --entrypoint bash floydcraft/cardano-node-iohk:latest
 else
   echo "please select a config to run: mainnet / testnet / mainbash / testbash"
 fi
