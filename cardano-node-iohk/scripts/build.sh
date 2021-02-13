92807d9079791d62497537aac66c8ba4e5e0528c
Bourke Floyd <chbfiv@gmail.com>
2021-02-13 13:12:28 -0800

    moving docker registry to dockerhub (public is fine)

--- cardano-node-iohk/scripts/build.sh
+++ cardano-node-iohk/scripts/build.sh
@@ -3,8 +3,12 @@ set -e
 set -u
 set -o pipefail
 
+COMMIT_SHA=$(git rev-parse HEAD)
 VERSION="1.25.1"
 docker build \
+    --memory=8g \
     --build-arg VERSION=${VERSION} \
+    --tag floydcraft/cardano-node-iohk:${COMMIT_SHA} \
     --tag floydcraft/cardano-node-iohk:${VERSION} \
     --tag floydcraft/cardano-node-iohk:latest .
+
--- cardano-node-iohk/scripts/run.sh
+++ cardano-node-iohk/scripts/run.sh
@@ -13,6 +13,10 @@ elif [[ "$PARAM" == "mainbash" ]]; then
   docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=mainnet -p 3001:3001 --entrypoint bash floydcraft/cardano-node-iohk:latest
 elif [[ "$PARAM" == "testbash" ]]; then
   docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=testnet -p 3001:3001 --entrypoint bash floydcraft/cardano-node-iohk:latest
+elif [[ "$PARAM" == "iohktestbash" ]]; then
+  docker run -it -v $PWD/storage:/storage --env CARDANO_NETWORK=testnet -p 3001:3001 --entrypoint bash floydcraft/cardano-node-iohk:latest
 else
   echo "please select a config to run: mainnet / testnet / mainbash / testbash"
 fi
+
+
