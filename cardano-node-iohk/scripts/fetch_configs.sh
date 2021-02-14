53de087acb94e8ff045afee8d42b38f3ddef32a7
Bourke Floyd <chbfiv@gmail.com>
2021-02-14 13:00:35 -0800

    removed "staging"

--- cardano-node-iohk/scripts/fetch_configs.sh
+++ cardano-node-iohk/scripts/fetch_configs.sh
@@ -18,10 +18,3 @@ curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/la
 curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-topology.json
 curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-db-sync-config.json
 curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-rest-config.json
-
-curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/staging-config.json
-curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/staging-byron-genesis.json
-curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/staging-shelley-genesis.json
-curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/staging-topology.json
-curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/staging-db-sync-config.json
-curl -L -O  https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/staging-rest-config.json
--- cardano-node-iohk/stage3-files/scripts/entrypoint.sh
+++ cardano-node-iohk/stage3-files/scripts/entrypoint.sh
@@ -7,23 +7,16 @@ if [[ "$CARDANO_NETWORK" == "mainnet" ]]; then
   cardano-node  run --config "/config/mainnet-config.json" \
     --topology "/config/mainnet-topology.json" \
     --database-path "/storage/mainnet/db" \
-    --socket-path "/storage/mainnet/sockets/nodes.socket" \
     --host-addr "0.0.0.0" \
-    --port 3001
+    --port 3001 \
+    --socket-path "/storage/mainnet/sockets/nodes.socket"
 elif [[ "$CARDANO_NETWORK" == "testnet" ]]; then
   cardano-node  run --config "/config/testnet-config.json" \
     --topology "/config/testnet-topology.json" \
     --database-path "/storage/testnet/db" \
-    --socket-path "/storage/testnet/sockets/nodes.socket" \
     --host-addr "0.0.0.0" \
-    --port 3001
-elif [[ "$CARDANO_NETWORK" == "staging" ]]; then
-  cardano-node  run --config "/config/staging-config.json" \
-    --topology "/config/staging-topology.json" \
-    --database-path "/storage/staging/db" \
-    --socket-path "/storage/staging/sockets/nodes.socket" \
-    --host-addr "0.0.0.0" \
-    --port 3001
+    --port 3001 \
+    --socket-path "/storage/testnet/sockets/nodes.socket"
 else
   echo "Please set a CARDANO_NETWORK environment variable to one of the following: mainnet, testnet, or staging"
 fi
