3d64e196d9114379d0da9ab3a9b3db74e847cb27
Bourke Floyd <chbfiv@gmail.com>
2021-02-14 21:17:56 -0800

    improved dev scripts to create or attach to container

--- cardano-node-iohk/scripts/run.sh
+++ cardano-node-iohk/scripts/run.sh
@@ -3,33 +3,25 @@
 #set -u
 #set -o pipefail
 
-OPTION1=$1
-OPTION2=$2
-OPTION3=$3
-
-if [[ "$OPTION1" == "mainnet" ]]; then
-  echo "CARDANO_NETWORK=$OPTION1"
-elif [[ "$OPTION1" == "testnet" ]]; then
-  echo "CARDANO_NETWORK=$OPTION1"
-elif [[ "$OPTION1" == "staging" ]]; then
-  echo "CARDANO_NETWORK=$OPTION1"
+if [[ ("$1" == "mainnet") || ("$1" == "testnet") ]]; then
+  CARDANO_NETWORK="$1"
 else
-  echo "please select an option: mainnet / testnet / staging"
+  echo "please select an option: mainnet, or testnet "
+  exit 1
 fi
 
-if [[ "$OPTION2" == "pull" ]]; then
+if [[ "$2" == "pull" ]]; then
   docker pull floydcraft/cardano-node-iohk:latest
 fi
 
-if [[ ("$OPTION2" == "bash") || ("$OPTION3" == "bash") ]]; then
-  docker run -it -v $PWD/storage:/storage --env "CARDANO_NETWORK=$OPTION1" --entrypoint bash floydcraft/cardano-node-iohk:latest
+if [ "$( docker container inspect -f '{{.State.Running}}' cardano_node_iohk )" == "true" ]; then
+  printf "active container found for: cardano_node_iohk\nattaching to the container\n"
+  docker exec -it cardano_node_iohk bash
 else
-  docker run -it -v $PWD/storage:/storage --env "CARDANO_NETWORK=$OPTION1" floydcraft/cardano-node-iohk:latest
+  printf "no active container found for: cardano_node_iohk\ncleaning containers and creating new container via run\n"
+  docker container prune -f
+  docker run --name cardano_node_iohk -it -v $PWD/storage:/storage --env "CARDANO_NETWORK=$CARDANO_NETWORK" --entrypoint bash floydcraft/cardano-node-iohk:latest
 fi
-
-
-
-
 
 
 
