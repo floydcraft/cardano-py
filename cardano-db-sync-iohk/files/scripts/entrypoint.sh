8b3ed283724478a2646e0d6777f9591951356f6a
Bourke Floyd <chbfiv@gmail.com>
2021-02-20 00:55:30 -0800

    added cardano db sync baseline setup

--- cardano-db-sync-iohk/files/scripts/entrypoint.sh
+++ cardano-db-sync-iohk/files/scripts/entrypoint.sh
@@ -3,5 +3,22 @@ set -e
 set -u
 set -o pipefail
 
-pg_ctlcluster 13 main start
-postgres createuser --createdb --superuser runner
+export PGPASSFILE="/storage/$CARDANO_NETWORK.pgpass"
+
+pg_ctlcluster 11 main restart
+
+if [[ "$CARDANO_NETWORK" == "mainnet" ]]; then
+  cardano-db-sync \
+    --config "/storage/config/mainnet-db-sync-config.json" \
+    --socket-path "/storage/node.socket" \
+    --state-dir "/storage/db-sync-node/ledger-state/mainnet" \
+    --schema-dir "schema/"
+elif [[ "$CARDANO_NETWORK" == "testnet" ]]; then
+  cardano-db-sync \
+    --config "/storage/config/mainnet-db-sync-config.json" \
+    --socket-path "/storage/node.socket" \
+    --state-dir "/storage/db-sync-node/ledger-state/testnet" \
+    --schema-dir "schema/"
+else
+  echo "Please set a CARDANO_NETWORK environment variable to one of the following: mainnet or testnet"
+fi
