b2f3b82bea1af856945a30de509e159513164a0b
Bourke Floyd <chbfiv@gmail.com>
2021-02-28 14:20:13 -0800

    fix typo in entrypoint.sh

--- cardano-node-iohk/files/scripts/entrypoint.sh
+++ cardano-node-iohk/files/scripts/entrypoint.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 if [[ "$CARDANO_NODE_TYPE" == "producer" ]]; then
   TOPOLOGY_CONFIG="/config/$CARDANO_NETWORK/$CARDANO_NODE_TYPE-topology.json"
-elif [[ "$1" == "relay" ]]; then
+elif [[ "$CARDANO_NODE_TYPE" == "relay" ]]; then
   TOPOLOGY_CONFIG="/config/$CARDANO_NETWORK/$CARDANO_NODE_TYPE-$TARGET_ROLE-topology.json"
 else
   printf "please select an option (cardano node type): producer or relay"
