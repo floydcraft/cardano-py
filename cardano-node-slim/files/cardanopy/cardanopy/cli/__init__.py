d3b72ce5e3c14e2187e8a5bd104a771886266b3c
Bourke Floyd <chbfiv@gmail.com>
2021-03-01 22:45:08 -0800

    (CardanoPy) added address key-gen wrapper CLI

--- cardano-node-slim/files/cardanopy/cardanopy/cli/__init__.py
+++ cardano-node-slim/files/cardanopy/cardanopy/cli/__init__.py
@@ -1,6 +1,7 @@
 import click
 
 from .commands.query import query
+from .commands.address import address
 
 @click.group()
 @click.version_option(version='0.1.0')
@@ -10,3 +11,4 @@ def cli(ctx):
 
 # export
 cli.add_command(query)
+cli.add_command(address)
---
+++ cardano-node-slim/files/cardanopy/cardanopy/cli/commands/address.py
@@ -0,0 +1,23 @@
+import click
+import subprocess
+
+@click.group()
+@click.pass_context
+def address(ctx):
+    """Payment address commands"""
+
+@address.command("key-gen")
+@click.option('--normal-key', 'key_type', flag_value='normal-key', default=True, help='Use a normal Shelley-era key (default).')
+@click.option('--extended-key', 'key_type', flag_value='extended-key', help='Use an extended ed25519 Shelley-era key.')
+@click.option('--byron-key', 'key_type', flag_value='byron-key', help='Use a Byron-era key.')
+@click.option('--verification-key-file', 'verification_key_file', required=True, type=str, help='Output filepath of the verification key.')
+@click.option('--signing-key-file', 'signing_key_file', required=True, type=str, help='Output filepath of the signing key.')
+def key_gen(key_type, verification_key_file, signing_key_file):
+    """Get the node's current tip (slot no, hash, block no)."""
+    subprocess.run(["cardano-cli",
+                    "address",
+                    "key-gen",
+                    f"--{key_type}",
+                    "--verification-key-file", verification_key_file,
+                    "--signing-key-file", signing_key_file
+                    ])
