b425b68d26812c849a8d25ab54bab0f531ce7de2
Bourke Floyd <chbfiv@gmail.com>
2021-03-03 19:55:46 -0800

    made node relays explicit instead of dynamic

--- cardano-node-iohk/files/scripts/entrypoint.sh
+++ cardano-node-iohk/files/scripts/entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 set -u
 set -o pipefail
 
+export POD_INDEX=${HOSTNAME##*-}
 if [[ "$CARDANO_NODE_TYPE" == "producer" ]]; then
-  TOPOLOGY_CONFIG="/config/$CARDANO_NETWORK/$CARDANO_NODE_TYPE-topology.json"
-elif [[ "$CARDANO_NODE_TYPE" == "relay" ]]; then
   TOPOLOGY_CONFIG="/config/$CARDANO_NETWORK/$CARDANO_NODE_TYPE-$TARGET_ROLE-topology.json"
+elif [[ "$CARDANO_NODE_TYPE" == "relay" ]]; then
+  TOPOLOGY_CONFIG="/config/$CARDANO_NETWORK/$CARDANO_NODE_TYPE-$TARGET_ROLE-$POD_INDEX-topology.json"
 else
   printf "please select an option (cardano node type): producer or relay"
   exit 1
