df4d24883a388a4d09f1744e4479f4d8afd2efdc
Bourke Floyd <chbfiv@gmail.com>
2021-03-05 19:32:17 -0800

    simplify upload for pkg

--- cardano-py-pkg/setup.py
+++ cardano-py-pkg/setup.py
@@ -8,7 +8,7 @@ setup(name='cardanopy',
       author='Bourke Floyd',
       author_email='chbfiv@floydcraft.com',
       url='https://github.com/floydcraft/cardano-py',
-      packages=find_packages(exclude=['scripts', 'dist', 'build', 'cardanopy.egg-info']),
+      packages=find_packages(),
       keywords='cardano,ada,cli',
       python_requires='>=3.5.3,<4',
       install_requires=[
