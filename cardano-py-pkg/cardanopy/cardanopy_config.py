39246237e09825b5adb647b3bb37f5d23e3fe5b6
Bourke Floyd <chbfiv@gmail.com>
2021-03-11 17:32:26 -0800

    added CardanoPy config test

--- cardano-py-pkg/cardanopy/cardanopy_config.py
+++ cardano-py-pkg/cardanopy/cardanopy_config.py
@@ -41,17 +41,22 @@ class CardanoPyConfig(object):
             target_config = target_config_dir
             target_config_dir = target_config_dir.parent
 
-        if not target_config.is_file():
-            raise ValueError(f"Target config '{target_config}' is not a file. e.g., 'cardanopy.yaml'")
-
-        if not target_config.exists():
-            raise ValueError(f"Target file '{target_config}' does not exist.")
+        # if not target_config.is_file():
+        #     raise ValueError(f"Target config '{target_config}' is not a file. e.g., 'cardanopy.yaml'")
+        #
+        # if not target_config.exists():
+        #     raise ValueError(f"Target file '{target_config}' does not exist.")
 
         try:
             with open(target_config, "w") as file:
                 yaml.dump(self.target_config_yaml, file)
         except Exception as ex:
             raise ValueError(f"Failed to create. Unable to copy config to '{target_config_dir}'. {type(ex).__name__} {ex.args}")
+
+    def get_api_version(self):
+        return self.target_config_yaml['apiVersion']
+
+    apiVersion = property(get_api_version)
 
     def get_substitutions(self):
         return self.target_config_yaml['substitutions']
@@ -113,6 +118,11 @@ class CardanoPyConfig(object):
 
     docker = property(get_docker)
 
+    def get_kubernetes(self):
+        return KubernetesConfig(self.target_config_yaml['kubernetes'])
+
+    kubernetes = property(get_kubernetes)
+
 
 class DockerConfig(object):
     config = None
@@ -124,3 +134,15 @@ class DockerConfig(object):
         return self.config['image']
 
     image = property(get_image)
+
+
+class KubernetesConfig(object):
+    config = None
+
+    def __init__(self, config):
+        self.config = config
+
+    def get_namespace(self):
+        return self.config['namespace']
+
+    namespace = property(get_namespace)
---
+++ cardano-py-pkg/cardanopy/tests/test_cardanopy_config.py
@@ -0,0 +1,55 @@
+import unittest
+from click.testing import CliRunner
+from ..cardanopy_config import CardanoPyConfig
+from pathlib import Path
+import tempfile
+import shutil
+import os
+
+
+class TestCreate(unittest.TestCase):
+
+    def setUp(self):
+        self.test_dir = Path(tempfile.mkdtemp())
+        os.chdir(self.test_dir)
+        # print(f"test_dir='{self.test_dir}'")
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_basic_config(self):
+        file = Path(__file__).parent.joinpath("../templates").absolute() / "testnet/basic.yaml"
+        cardanopy_config = CardanoPyConfig()
+        cardanopy_config.load(file)
+
+        self.assertEqual(cardanopy_config.apiVersion, "cardanopy.node.config/v1")
+        self.assertEqual(cardanopy_config.name, "basic")
+        self.assertEqual(cardanopy_config.network, "testnet")
+        self.assertEqual(cardanopy_config.root, "/app")
+        self.assertEqual(cardanopy_config.config, "/app/config/config.json")
+        self.assertEqual(cardanopy_config.topologyPath, "/app/config/topology.json")
+        self.assertEqual(cardanopy_config.databasePath, "/app/db")
+        self.assertEqual(cardanopy_config.socketPath, "/app/node.socket")
+        self.assertEqual(cardanopy_config.hostAddr, "0.0.0.0")
+        self.assertEqual(cardanopy_config.port, 3001)
+        self.assertEqual(cardanopy_config.docker.image, "floydcraft/cardano-py-slim:latest")
+        self.assertEqual(cardanopy_config.kubernetes.namespace, "cardano-testnet")
+
+        file_save = self.test_dir / (file.name)
+        cardanopy_config.save(file_save)
+
+        cardanopy_config_v2 = CardanoPyConfig()
+        cardanopy_config_v2.load(file_save)
+
+        self.assertEqual(cardanopy_config_v2.apiVersion, cardanopy_config.apiVersion)
+        self.assertEqual(cardanopy_config_v2.name, cardanopy_config.name)
+        self.assertEqual(cardanopy_config_v2.network, cardanopy_config.network)
+        self.assertEqual(cardanopy_config_v2.root, cardanopy_config.root)
+        self.assertEqual(cardanopy_config_v2.config, cardanopy_config.config)
+        self.assertEqual(cardanopy_config_v2.topologyPath, cardanopy_config.topologyPath)
+        self.assertEqual(cardanopy_config_v2.databasePath, cardanopy_config.databasePath)
+        self.assertEqual(cardanopy_config_v2.socketPath, cardanopy_config.socketPath)
+        self.assertEqual(cardanopy_config_v2.hostAddr, cardanopy_config.hostAddr)
+        self.assertEqual(cardanopy_config_v2.port, cardanopy_config.port)
+        self.assertEqual(cardanopy_config_v2.docker.image, cardanopy_config.docker.image)
+        self.assertEqual(cardanopy_config_v2.kubernetes.namespace, cardanopy_config.kubernetes.namespace)
