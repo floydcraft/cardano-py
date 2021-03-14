94419a20c175937af5e5d131d88cc7c144bd4ff9
Bourke Floyd <chbfiv@gmail.com>
2021-03-14 09:03:17 -0700

    Bump version: 0.1.6-dev13 → 0.1.6-dev14

--- cardano-py-pkg/setup.py
+++ cardano-py-pkg/setup.py
@@ -7,7 +7,7 @@ datadir = Path(__file__).parent / 'cardanopy' / 'data'
 files = ['data/' + str(p.relative_to(datadir)) for p in datadir.rglob('*')]
 
 setup(name='cardanopy',
-      version='0.1.6-dev13',
+      version='0.1.6-dev14',
       description='Cardano CLI tools for python3',
       author='Bourke Floyd',
       author_email='chbfiv@floydcraft.com',
