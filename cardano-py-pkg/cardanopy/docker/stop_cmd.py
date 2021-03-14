4a3ade495465ee5cdf1a1704c0afbac740b05572
Bourke Floyd <chbfiv@gmail.com>
2021-03-13 17:11:42 -0800

    cleanup docker stop command with new docker helpers

--- cardano-py-pkg/cardanopy/docker/stop_cmd.py
+++ cardano-py-pkg/cardanopy/docker/stop_cmd.py
@@ -2,72 +2,27 @@ import click
 import subprocess
 from pathlib import Path
 from ..core.cardanopy_config import CardanoPyConfig
+from .docker_helper import DockerHelper
 
 
 @click.command("stop")
 @click.option('-r', '--dry-run', 'dry_run', is_flag=True, help="print the mutable commands")
-@click.argument('target_config_dir', type=str)
+@click.argument('target_config_dir_or_file', type=str)
 @click.pass_context
-def stop_cmd(ctx, dry_run, target_config_dir):
-    """Docker Bash helper command"""
-
-    if dry_run:
-        print("#### DRY RUN - no mutable changes will be made. ####")
-
-    target_config_dir = Path(target_config_dir)
-
-    cardanopy_config = CardanoPyConfig()
-    try:
-        cardanopy_config.load(target_config_dir)
-    except ValueError as e:
-        ctx.fail(e.args)
-        return 1
+def stop_cmd(ctx, dry_run, target_config_dir_or_file):
+    """Docker Stop helper command"""
 
     try:
-        result = subprocess.run(["docker",
-                        "ps",
-                        "-q",
-                        "-f", f"name={cardanopy_config.docker.name}"],
-                        stdout=subprocess.PIPE).stdout.decode('utf-8')
-        container_running = len(result) > 0
+        target_config_file = CardanoPyConfig.try_get_valid_config_file(target_config_dir_or_file)
+
+        cardanopy_config = CardanoPyConfig()
+        cardanopy_config.load(target_config_file)
+
+        if DockerHelper.is_container_running(cardanopy_config.docker.name, dry_run):
+            DockerHelper.stop_container(cardanopy_config.docker.name, dry_run)
+
+        if DockerHelper.is_container_exited(cardanopy_config.docker.name, dry_run):
+            DockerHelper.remove_container(cardanopy_config.docker.name, dry_run)
     except Exception as ex:
-        ctx.fail(f"Unknown exception: {type(ex).__name__} {ex.args}")
+        ctx.fail(f"docker:stop_cmd(dry_run={dry_run}, target_config_dir_or_file='{target_config_dir_or_file}') failed: {type(ex).__name__} {ex.args}")
         return 1
-
-    if container_running:
-        docker_stop_cmd = ["docker",
-                           "stop",
-                           cardanopy_config.docker.name]
-        if dry_run:
-            print(" ".join(docker_stop_cmd))
-        else:
-            try:
-                subprocess.run(docker_stop_cmd)
-            except Exception as ex:
-                ctx.fail(f"Unknown exception: {type(ex).__name__} {ex.args}")
-                return 1
-
-    try:
-        result = subprocess.run(["docker",
-                                 "ps",
-                                 "-aq",
-                                 "-f", f"name={cardanopy_config.docker.name}"],
-                                stdout=subprocess.PIPE).stdout.decode('utf-8')
-        container_exited = len(result) > 0
-    except Exception as ex:
-        ctx.fail(f"Unknown exception: {type(ex).__name__} {ex.args}")
-        return 1
-
-    if container_exited:
-        docker_container_rm_cmd = ["docker",
-                                    "container",
-                                    "rm", cardanopy_config.docker.name]
-        if dry_run:
-            print(" ".join(docker_container_rm_cmd))
-        else:
-            try:
-                subprocess.run(docker_container_rm_cmd)
-            except Exception as ex:
-                ctx.fail(f"Unknown exception: {type(ex).__name__} {ex.args}")
-                return 1
-
