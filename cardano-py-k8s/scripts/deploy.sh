cbd05b4b738b72fbdae40307a0296997b9757394
Bourke Floyd <chbfiv@gmail.com>
2021-03-14 19:04:57 -0700

    merge rebase changes

--- cardano-py-k8s/scripts/deploy.sh
+++ cardano-py-k8s/scripts/deploy.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 mkdir -p bin
 
-helm template --set namespace=cardano-mainnet --set cardanoNetwork=mainnet . > bin/mainnet.yaml
-helm template --set namespace=cardano-testnet --set cardanoNetwork=testnet . > bin/testnet.yaml
+# helm template . > bin/mainnet.yaml
+helm template . > bin/testnet.yaml
 #kubectl apply -n cardano-mainnet -f bin/mainnet.yaml
 kubectl apply -n cardano-testnet -f bin/testnet.yaml
