6402059bd9540d2d8712f5b86d457c4f49b01be0
Bourke Floyd <chbfiv@gmail.com>
2021-03-25 09:36:24 -0700

    Revert "Ingress version"
    This reverts commit 8222bdcb07a66ade1459fd3f5a7393507d481557.

--- cardano-py-k8s/scripts/deploy.sh
+++ cardano-py-k8s/scripts/deploy.sh
@@ -6,12 +6,12 @@ set -o pipefail
 mkdir -p bin
 
 helm template --set namespace="cardano-mainnet" \
-  --set cardanoNode.relayIpv4="cardanopy-relay-mainnet-ipv4" \
-  --set cardanoNode.relayIpv6="cardanopy-relay-mainnet-ipv6" \
+  --set cardanoNode.relayIpv4="34.107.174.232" \
+  --set cardanoNode.relayIpv6="2600:1901:0:1ac5::" \
   --set cardanoNode.network="mainnet" . > bin/mainnet.yaml
 helm template --set namespace="cardano-testnet" \
-  --set cardanoNode.relayIpv4="cardanopy-relay-testnet-ipv4" \
-  --set cardanoNode.relayIpv6="cardanopy-relay-testnet-ipv6" \
+  --set cardanoNode.relayIpv4="34.120.114.40" \
+  --set cardanoNode.relayIpv6="2600:1901:0:686c::" \
   --set cardanoNode.network="testnet" . > bin/testnet.yaml
 kubectl apply -n cardano-mainnet -f bin/mainnet.yaml
 kubectl apply -n cardano-testnet -f bin/testnet.yaml
