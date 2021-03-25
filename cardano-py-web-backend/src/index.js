071f2f14171890cdb933f0e670174d278bfc1071
Bourke Floyd <chbfiv@gmail.com>
2021-03-24 23:40:00 -0700

    changing description (final)

--- cardano-py-web-backend/src/index.js
+++ cardano-py-web-backend/src/index.js
@@ -30,7 +30,7 @@ app.get('/api/stakepool/metadata', function (req, res) {
     }
     let data = {
         "name": name,
-        "description": "pip3 install --upgrade cardanopy # Cardano python3 CLI -> profit!",
+        "description": "pip3 install --upgrade cardanopy # Cardano python3 CLI -> Cardano dApp and ETL tools!",
         "ticker": "₳Py🥧",
         "homepage": "https://cardanopy.com/?utm_source=" + process.env.CARDANO_NETWORK + "&utm_medium=metadata&utm_campaign=v1"
     }
