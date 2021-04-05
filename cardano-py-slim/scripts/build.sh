971d08ea7ea55426a1463328966d7c1c150c5f82
Bourke Floyd <chbfiv@gmail.com>
2021-04-05 00:34:43 -0700

    Bump version: 0.1.7-dev21 → 0.1.7-dev22

--- cardano-py-slim/scripts/build.sh
+++ cardano-py-slim/scripts/build.sh
@@ -3,4 +3,4 @@ set -e
 set -u
 set -o pipefail
 
-docker build --tag "floydcraft/cardano-py-slim:0.1.7-dev21" .
+docker build --tag "floydcraft/cardano-py-slim:0.1.7-dev22" .
--- cardano-py-slim/scripts/run.sh
+++ cardano-py-slim/scripts/run.sh
@@ -4,7 +4,7 @@
 #set -o pipefail
 
 IMAGE=cardano-py-slim
-IMAGE_TAG=0.1.7-dev21
+IMAGE_TAG=0.1.7-dev22
 
 if [[ "$1" == "pull" ]]; then
   docker pull "floydcraft/$IMAGE:$IMAGE_TAG"
