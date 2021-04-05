422e8b1a5f0b27501c5f3800e91a810c70bc49f4
Bourke Floyd <chbfiv@gmail.com>
2021-04-04 22:05:33 -0700

    added secret scripts for kubernetes

---
+++ cardano-py-k8s/scripts/create_secret.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+mkdir -p bin
+
+kubectl create secret generic -n cardano-testnet \
+  block-producer-credentials \
+  --from-file files/bp/kes.skey \
+  --from-file files/bp/node.cert \
+  --from-file files/bp/vrf.skey
