23ac83c1b51af5667d209341c5208e3dfdf8142a
Bourke Floyd <chbfiv@gmail.com>
2021-04-05 22:07:33 -0700

    changed to e2 pool from n2 to bin pack more

--- cardano-py-k8s/scripts/create_secret.sh
+++ cardano-py-k8s/scripts/create_secret.sh
@@ -5,8 +5,13 @@ set -o pipefail
 
 mkdir -p bin
 
-kubectl create secret generic -n cardano-testnet \
-  block-producer-credentials \
-  --from-file files/bp/kes.skey \
-  --from-file files/bp/node.cert \
-  --from-file files/bp/vrf.skey
+#kubectl create secret generic -n cardano-mainnet \
+#  block-producer-credentials \
+#  --from-file files/bp/kes.skey \
+#  --from-file files/bp/node.cert \
+#  --from-file files/bp/vrf.skey
+#kubectl create secret generic -n cardano-testnet \
+#  block-producer-credentials \
+#  --from-file files/bp/kes.skey \
+#  --from-file files/bp/node.cert \
+#  --from-file files/bp/vrf.skey
--- cardano-py-k8s/scripts/deploy.sh
+++ cardano-py-k8s/scripts/deploy.sh
@@ -7,15 +7,15 @@ mkdir -p bin
 
 helm template --set namespace="cardano-mainnet" \
   --set cardanoNode.relayIpv4="35.184.53.16" \
-  --set cardanoNode.memoryRequest="7Gi" \
+  --set cardanoNode.memoryRequest="4.5Gi" \
   --set cardanoNode.storageRequest="50Gi" \
   --set cardanoNode.relayDns="mainnet-relays.cardanopy.com" \
   --set cardanoNode.network="mainnet" . > bin/mainnet.yaml
 helm template --set namespace="cardano-testnet" \
   --set cardanoNode.relayIpv4="34.68.36.180" \
-  --set cardanoNode.memoryRequest="2Gi" \
+  --set cardanoNode.memoryRequest="500Mi" \
   --set cardanoNode.storageRequest="10Gi" \
   --set cardanoNode.relayDns="testnet-relays.cardanopy.com" \
   --set cardanoNode.network="testnet" . > bin/testnet.yaml
-#kubectl apply -n cardano-mainnet -f bin/mainnet.yaml
+kubectl apply -n cardano-mainnet -f bin/mainnet.yaml
 kubectl apply -n cardano-testnet -f bin/testnet.yaml
