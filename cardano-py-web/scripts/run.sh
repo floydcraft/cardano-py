80949164cf3a0dd66d39c533521e425228b99a8a
Bourke Floyd <chbfiv@gmail.com>
2021-04-15 23:59:09 -0700

    web landing page upgrades

--- cardano-py-web/scripts/run.sh
+++ cardano-py-web/scripts/run.sh
@@ -3,23 +3,24 @@
 #set -u
 #set -o pipefail
 
-IMAGE=cardano-py-web
+IMAGE="cardano-py-web"
+CONTAINER_NAME="cpy-web"
 
-if [[ "$1" == "pull" ]]; then
-  docker pull floydcraft/$IMAGE:latest
+if [[ ! -z $( docker ps -q -f "name=^/$CONTAINER_NAME$" ) ]]; then
+  printf "Active container  found for: $IMAGE\nattaching to the container '$CONTAINER_NAME'\n"
+  docker exec -it $CONTAINER_NAME /bin/sh
+  exit 0
 fi
 
-printf "IMAGE=$IMAGE\n"
-
-if [[ "$( docker container inspect -f '{{.State.Running}}' "$IMAGE" )" == "true" ]]; then
-  printf "ACTIVE CONTAINER found for: $IMAGE\nattaching to the container\n"
-  docker exec -it "$IMAGE" /bin/sh
+if [[ ! -z $( docker ps -aq -f "name=^/$CONTAINER_NAME$" ) ]]; then
+  printf "Inactive container found for: $IMAGE\nstarting the container '$CONTAINER_NAME'\n"
+  docker start $CONTAINER_NAME
+  docker exec -it $CONTAINER_NAME /bin/sh
 else
-  printf "NO ACTIVE CONTAINER found for: $IMAGE\ncleaning containers and creating new container via run\n"
-  docker rm "$IMAGE"
-  docker run --name "$IMAGE" -d \
-    -p "8080:8080" "floydcraft/$IMAGE:latest"
-  docker exec -it "$IMAGE" /bin/sh
+  printf "No active container found for: $IMAGE\nrunning a new container '$CONTAINER_NAME'\n"
+  docker run --name $CONTAINER_NAME \
+    -d \
+    -p 8080:8080 \
+    "floydcraft/$IMAGE:latest"
+  docker exec -it $CONTAINER_NAME /bin/sh
 fi
-
-
