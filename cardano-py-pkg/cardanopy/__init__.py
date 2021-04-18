fca45d2b03c1172d5ae6a62440b1a16298618d91
Bourke Floyd <chbfiv@gmail.com>
2021-04-18 06:16:04 -0700

    Bump version: 0.1.8 → 0.1.9-dev1

--- cardano-py-pkg/cardanopy/__init__.py
+++ cardano-py-pkg/cardanopy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.8"
+__version__ = "0.1.9-dev1"
 __license__ = "Apache-2.0 License"
 __title__ = "cardanopy"
 
--- cardano-py-slim/scripts/run.sh
+++ cardano-py-slim/scripts/run.sh
@@ -4,7 +4,7 @@
 #set -o pipefail
 
 IMAGE=cardano-py-slim
-IMAGE_TAG=0.1.8
+IMAGE_TAG=0.1.9-dev1
 
 if [[ "$1" == "pull" ]]; then
   docker pull "floydcraft/$IMAGE:$IMAGE_TAG"
