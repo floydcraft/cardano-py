f271730417a285449cb224f26f4cb04cc8246a79
Bourke Floyd <chbfiv@gmail.com>
2021-04-18 06:15:26 -0700

    Bump version: 0.1.8-dev1 → 0.1.8

--- cardano-py-pkg/cardanopy/__init__.py
+++ cardano-py-pkg/cardanopy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.8-dev1"
+__version__ = "0.1.8"
 __license__ = "Apache-2.0 License"
 __title__ = "cardanopy"
 
--- cardano-py-slim/scripts/build.sh
+++ cardano-py-slim/scripts/build.sh
@@ -3,4 +3,4 @@ set -e
 set -u
 set -o pipefail
 
-docker build --tag "floydcraft/cardano-py-slim:0.1.8-dev1" .
+docker build --tag "floydcraft/cardano-py-slim:0.1.8" .
